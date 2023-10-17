880f053203591b811945af9f1ffdf37ed0ff3c8e
d-smith <doug.smith.mail@gmail.com>
2023-10-17 12:34:56 -0700

    factored out balances

--- main.go
+++ main.go
@@ -6,79 +6,12 @@ import (
 	"delsignrepl/state"
 	"delsignrepl/token"
 	"delsignrepl/wallets"
-	"encoding/json"
-	"fmt"
-	"math/big"
-	"net/http"
 
 	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
 
 var appToken string
-
-func getBalanceForAddress(address string) (*big.Int, error) {
-
-	req, err := http.NewRequest(http.MethodGet,
-		"http://localhost:3010/api/v1/wallets/balance/"+address, nil)
-	req.Header.Set("Authorization", "Bearer "+appToken)
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-
-	if resp.StatusCode != 200 {
-		return nil, fmt.Errorf("error retrieving wallets and addresses for user")
-	}
-
-	var accountBalance struct {
-		Address string   `json:"address"`
-		Amount  *big.Int `json:"amount"`
-	}
-
-	err = json.NewDecoder(resp.Body).Decode(&accountBalance)
-	if err != nil {
-		return nil, err
-	}
-
-	return accountBalance.Amount, nil
-}
-
-func weiToEther(val *big.Int) *big.Int {
-	return new(big.Int).Div(val, big.NewInt(1e18))
-}
-
-func doGetBalance(pages *tview.Pages) {
-	var msg string
-	if state.WalletId == 0 || state.Address == "" {
-		msg = "Wallet and address context not set"
-	} else {
-
-		balance, err := getBalanceForAddress(state.Address)
-		if err != nil {
-			msg = "Error retrieving balance: " + err.Error()
-		} else {
-			msg = fmt.Sprintf("Balance for wallet %d, address %s is %d wei (%d ether)",
-				state.WalletId, state.Address, balance, weiToEther(balance))
-		}
-
-	}
-
-	modal := tview.NewModal().
-		SetText(msg).
-		AddButtons([]string{"OK"}).
-		SetDoneFunc(func(buttonIndex int, buttonLabel string) {
-			if buttonLabel == "OK" {
-				pages.SwitchToPage("Menu")
-			}
-
-		})
-
-	pages.AddPage("Balance", modal, true, false)
-	pages.SwitchToPage("Balance")
-
-}
 
 func getMainList(pages *tview.Pages, app *tview.Application) *tview.List {
 	menuList := tview.NewList().
@@ -105,7 +38,7 @@ func getMainList(pages *tview.Pages, app *tview.Application) *tview.List {
 		} else if event.Rune() == 'c' {
 			state.DoSetWalletAndAccountCtx(pages, appToken)
 		} else if event.Rune() == 'b' {
-			doGetBalance(pages)
+			wallets.DoGetBalance(pages, state.WalletId, state.Address, appToken)
 		} else if event.Rune() == 's' {
 			send.ShowSendForm(pages, appToken)
 		}
